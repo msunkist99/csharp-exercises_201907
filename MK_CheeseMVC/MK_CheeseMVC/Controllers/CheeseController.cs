3d618acd187cc72faa174d8dff384f617c46173f
msunkist99 <msunkist99@gmail.com>
2019-12-22 16:11:33 -0600

    2019/12/22 - CheeseMVC initial code

---
+++ MK_CheeseMVC/MK_CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MK_CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View("Index");
+        }
+    }
+}
