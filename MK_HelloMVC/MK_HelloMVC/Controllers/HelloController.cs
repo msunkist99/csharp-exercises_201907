88088dd0e0f97981c0aa76965140a0ecfe496100
msunkist99 <msunkist99@gmail.com>
2019-12-22 11:44:16 -0600

    2019/12/22 - AskForLanguageBranch

--- MK_HelloMVC/MK_HelloMVC/Controllers/HelloController.cs
+++ MK_HelloMVC/MK_HelloMVC/Controllers/HelloController.cs
@@ -19,18 +19,35 @@ namespace MK_HelloMVC.Controllers
             string html = "<form method='post' >" +
                 "<input type='text' name='name' />" +
                 "<input type='submit' value='Greet me!' />" +
+                "<select name = 'language' id = 'language-select'>" +
+                "<option value = 'english' selected > English </option >" +
+                "<option value = 'french' > French </option >" + 
+                "<option value = 'spanish' > Spanish </option >" +
+                "<option value = 'german' > German </option >" +
+                "<option value = 'italian' > Italian </option >" +
+                "<option value = 'hindi' > Hindi </option >" +
+                "<option value = 'farsi' > Farsi </option >" + 
+                "</select >" +
                 "</form>";
 
-            //return Content(html, "text/html");
+            return Content(html, "text/html");
 
-            return Redirect("Hello/Goodbye");
+            //return Redirect("Hello/Goodbye");
         }
 
         [Route("/Hello")]
         [HttpPost]
-        public IActionResult Display(string name = "World")
+        public IActionResult Display(string name, string language)
         {
-            return Content(string.Format("<h1> Hello {0} </h1>", name), "text/html");
+
+            //return Content(string.Format("<h1> Hello {0} </h1>", name), "text/html");
+
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "World";
+            }
+
+            return Content(string.Format("<h1> {0}</h1>", CreateMessage(name, language)), "text/html");
         }
 
         [Route("Hello/{name}")]
@@ -42,6 +59,33 @@ namespace MK_HelloMVC.Controllers
         public IActionResult Goodbye()
         {
             return Content("Goodbye");
+        }
+
+        public static string CreateMessage (string name, string language)
+        {
+            switch (language)
+            {
+                case "french":
+                    return "Bonjour " + name;
+
+                case "spanish":
+                    return "Hola " + name;
+
+                case "german":
+                    return "Guten tag " + name;
+
+                case "italian":
+                    return "Caio " + name;
+
+                case "hindi":
+                    return "Namaste  " + name;
+
+                case "farsi":
+                    return "Salaam " + name;
+
+                default:
+                    return "Hello " + name;
+            }
         }
     }
 }
